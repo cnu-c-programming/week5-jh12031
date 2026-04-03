2c49484b1f6364bf611186acd1322b960ed51dd3
jh0100c <jh0100c@gmail.com>
2026-04-03 09:45:15 +0900

    commit1

---
+++ code/1-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void print_reverse(int *arr, int size) {
+    int *p = arr + (size - 1);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d", *p);
+        p--;
+    }
+    
+}
+
+int main() {
+    int nums[] = {1, 2, 3, 4, 5};
+
+    print_reverse(nums, 5);
+    printf("\n");
+
+    return 0;
+}
